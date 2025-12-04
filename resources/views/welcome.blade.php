c280f5f91f98c215d85b4577f76210dcbe0bacad
AmosManurung <iss23027@students.del.ac.id>
2025-12-04 13:18:28 +0700

    uptdate detail Produk

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -355,9 +355,9 @@
 
                                 <!-- Price -->
                                 <div class="mt-3">
-                                    <div class="text-red-600 font-semibold text-base">{{ $product['formatted_price'] }}</div>
-                                    @if($product['formatted_original_price'])
-                                        <div class="text-xs text-gray-400 line-through mt-0.5">{{ $product['formatted_original_price'] }}</div>
+                                    <div class="text-red-600 font-semibold text-base">{{ $product['price'] }}</div>
+                                    @if(isset($product['original_price']) && $product['original_price'])
+                                        <div class="text-xs text-gray-400 line-through mt-0.5">{{ $product['original_price'] }}</div>
                                     @endif
                                 </div>
 
@@ -368,7 +368,7 @@
                                     <form method="GET" action="{{ route('tambah.ke.keranjang') }}" class="flex">
                                         <button type="submit"
                                             data-name="{{ $product['name'] }}"
-                                            data-price="{{ $product['formatted_price'] }}"
+                                            data-price="{{ $product['price'] }}"
                                             class="btn-add-to-cart flex-1 inline-flex items-center justify-center gap-2 px-3 py-2 bg-red-700 text-white rounded-md hover:bg-red-800 transition text-sm font-medium">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4" />
