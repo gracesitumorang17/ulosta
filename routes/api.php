1d4f6aa6873331b51e78a5037418bb4b46ee9303
AmosManurung <iss23027@students.del.ac.id>
2025-12-14 09:47:44 +0700

    menyelesaikan API

--- routes/api.php
+++ routes/api.php
@@ -11,9 +11,9 @@ use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\ProfileController;
 
 /*
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------
 | API Routes
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application.
 | These routes are loaded by the RouteServiceProvider and all of them will
